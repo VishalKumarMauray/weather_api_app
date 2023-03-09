f5bd148d3a4e92380688e0385f88abfe130a872f
vishal kumar mauray <vishalmauray18@gmail.com>
2023-03-09 13:31:51 +0530

    dynamic api call..

--- lib/home.screen.dart
+++ lib/home.screen.dart
@@ -4,13 +4,15 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<WeatherData> fetchWeather() async {
+Future<WeatherData> fetchWeather(city) async {
   const String key = '03b130fd12f1f53657a2e0773740186e';
-  String city = '';
-  final response = await http.get(Uri.parse(
-      'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$key'));
-
+  print(city);
+  final response = await http.get(
+    Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$key'),
+  );
   if (response.statusCode == 200) {
+    print("response.statusCode ${response.statusCode}");
     return WeatherData.fromJson(jsonDecode(response.body));
   } else {
     throw Exception('Failed to load album');
@@ -187,10 +189,11 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late Future<WeatherData> futureWeather;
+  String city = '';
 
   @override
   void initState() {
-    futureWeather = fetchWeather();
+    futureWeather = fetchWeather('delhi');
     super.initState();
   }
 
@@ -206,159 +209,202 @@ class _HomeScreenState extends State<HomeScreen> {
         appBar: AppBar(
           title: const Text('weather'),
         ),
-        body: Center(
-          child: FutureBuilder<WeatherData>(
-            future: futureWeather,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(25),
-                          border: Border.all(
-                            width: 2,
-                            color: Colors.grey,
+        body: FutureBuilder<WeatherData>(
+          future: futureWeather,
+          builder: (context, snapshot) {
+            print("connection state ${snapshot.connectionState}");
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                return const Center(child: CircularProgressIndicator());
+              case ConnectionState.waiting:
+                return const Center(child: CircularProgressIndicator());
+              case ConnectionState.active:
+                return const Center(child: CircularProgressIndicator());
+              case ConnectionState.done:
+                return Column(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25),
+                              border: Border.all(
+                                width: 2,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            margin: const EdgeInsets.all(16),
+                            padding: const EdgeInsets.symmetric(horizontal: 16),
+                            child: TextField(
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'enter the city...',
+                              ),
+                              onChanged: (value) {
+                                setState(() {
+                                  city = value;
+                                });
+                              },
+                            ),
                           ),
                         ),
-                        margin: const EdgeInsets.all(16),
-                        child: const TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
+                        Container(
+                          decoration: const BoxDecoration(
+                            color: Colors.blue,
+                            shape: BoxShape.circle,
+                          ),
+                          child: IconButton(
+                            onPressed: () {
+                              if (city.isNotEmpty) {
+                                setState(() {
+                                  futureWeather = fetchWeather(city);
+                                });
+                              }
+                            },
+                            icon: const Icon(Icons.arrow_forward),
                           ),
                         ),
+                        const SizedBox(width: 8),
+                      ],
+                    ),
+                    const SizedBox(height: 32),
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: snapshot.hasData
+                            ? Column(
+                                children: [
+                                  Text(
+                                    'lat: ${snapshot.data!.coord!.lat.toString()}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  Text(
+                                    'lon: ${snapshot.data!.coord!.lon.toString()}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  const SizedBox(height: 32),
+                                  Text(
+                                    'id: ${snapshot.data!.weather!.elementAt(0).id}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  Text(
+                                    'main: ${snapshot.data!.weather!.elementAt(0).main}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  Text(
+                                    'description: ${snapshot.data!.weather!.elementAt(0).description}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  Text(
+                                    'icon: ${snapshot.data!.weather!.elementAt(0).icon}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  const SizedBox(height: 32),
+                                  Text(
+                                    'base: ${snapshot.data!.base}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  const SizedBox(height: 32),
+                                  Text(
+                                    'feels like: ${snapshot.data!.main!.feels_like}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  Text(
+                                    'humidity: ${snapshot.data!.main!.humidity}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  Text(
+                                    'pressure: ${snapshot.data!.main!.pressure}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  Text(
+                                    'temp: ${snapshot.data!.main!.temp}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  Text(
+                                    'temp max: ${snapshot.data!.main!.temp_max}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  Text(
+                                    'temp min: ${snapshot.data!.main!.temp_min}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  const SizedBox(height: 32),
+                                  Text(
+                                    'visibility: ${snapshot.data!.visibility}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  const SizedBox(height: 32),
+                                  Text(
+                                    'speed: ${snapshot.data!.wind!.speed}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  Text(
+                                    'deg: ${snapshot.data!.wind!.deg}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  const SizedBox(height: 32),
+                                  Text(
+                                    'all: ${snapshot.data!.clouds!.all}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  const SizedBox(height: 32),
+                                  Text(
+                                    'dt: ${snapshot.data!.dt}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  const SizedBox(height: 32),
+                                  Text(
+                                    'country: ${snapshot.data!.sys!.country}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  Text(
+                                    'id: ${snapshot.data!.sys!.id}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  Text(
+                                    'sun rise: ${snapshot.data!.sys!.sunrise}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  Text(
+                                    'sun set: ${snapshot.data!.sys!.sunset}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  Text(
+                                    'type: ${snapshot.data!.sys!.type}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  const SizedBox(height: 32),
+                                  Text(
+                                    'timezone: ${snapshot.data!.timezone}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  const SizedBox(height: 32),
+                                  Text(
+                                    'id: ${snapshot.data!.id}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  const SizedBox(height: 32),
+                                  Text(
+                                    'name: ${snapshot.data!.name}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                  const SizedBox(height: 32),
+                                  Text(
+                                    'cod: ${snapshot.data!.cod}',
+                                    style: const TextStyle(fontSize: 24),
+                                  ),
+                                ],
+                              )
+                            : snapshot.hasError
+                                ? const Text("Please check the entered city!")
+                                : const CircularProgressIndicator(),
                       ),
-                      const SizedBox(height: 32),
-                      Text(
-                        'lat: ${snapshot.data!.coord!.lat.toString()}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      Text(
-                        'lon: ${snapshot.data!.coord!.lon.toString()}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      const SizedBox(height: 32),
-                      Text(
-                        'id: ${snapshot.data!.weather!.elementAt(0).id}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      Text(
-                        'main: ${snapshot.data!.weather!.elementAt(0).main}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      Text(
-                        'description: ${snapshot.data!.weather!.elementAt(0).description}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      Text(
-                        'icon: ${snapshot.data!.weather!.elementAt(0).icon}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      const SizedBox(height: 32),
-                      Text(
-                        'base: ${snapshot.data!.base}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      const SizedBox(height: 32),
-                      Text(
-                        'feels like: ${snapshot.data!.main!.feels_like}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      Text(
-                        'humidity: ${snapshot.data!.main!.humidity}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      Text(
-                        'pressure: ${snapshot.data!.main!.pressure}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      Text(
-                        'temp: ${snapshot.data!.main!.temp}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      Text(
-                        'temp max: ${snapshot.data!.main!.temp_max}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      Text(
-                        'temp min: ${snapshot.data!.main!.temp_min}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      const SizedBox(height: 32),
-                      Text(
-                        'visibility: ${snapshot.data!.visibility}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      const SizedBox(height: 32),
-                      Text(
-                        'speed: ${snapshot.data!.wind!.speed}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      Text(
-                        'deg: ${snapshot.data!.wind!.deg}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      const SizedBox(height: 32),
-                      Text(
-                        'all: ${snapshot.data!.clouds!.all}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      const SizedBox(height: 32),
-                      Text(
-                        'dt: ${snapshot.data!.dt}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      const SizedBox(height: 32),
-                      Text(
-                        'country: ${snapshot.data!.sys!.country}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      Text(
-                        'id: ${snapshot.data!.sys!.id}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      Text(
-                        'sun rise: ${snapshot.data!.sys!.sunrise}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      Text(
-                        'sun set: ${snapshot.data!.sys!.sunset}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      Text(
-                        'type: ${snapshot.data!.sys!.type}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      const SizedBox(height: 32),
-                      Text(
-                        'timezone: ${snapshot.data!.timezone}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      const SizedBox(height: 32),
-                      Text(
-                        'id: ${snapshot.data!.id}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      const SizedBox(height: 32),
-                      Text(
-                        'name: ${snapshot.data!.name}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                      const SizedBox(height: 32),
-                      Text(
-                        'cod: ${snapshot.data!.cod}',
-                        style: const TextStyle(fontSize: 24),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 );
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
-              return const CircularProgressIndicator();
-            },
-          ),
+            }
+          },
         ),
       ),
     );
